40bda10de30242f2ef4d8e2f9c90155b78f98dfb
chukwudiokolomansion <okolomansion@gmx.de>
2026-05-01 11:00:00 +0200

    dataTypes1

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const s5 = "and";
 
 // Concatenate the string variables into one new string
 
-
+const tongueTwister = s1+" "+s2+" "+s3+" "+s4+" "+s5
 // Print out the concatenated string
 
 
-
+console.log(tongueTwister)
 
 /*******************************************
     Iteration 1.2 | Camel Tail
@@ -22,11 +22,15 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
+const outcome =
+part1.slice(0, -1) +
+part1.slice(-1).toUpperCase() +
+part2.slice(0, -1) +
+part2.slice(-1).toUpperCase()
 
 // Print the cameLtaiL-formatted string
 
-
+console.log(outcome)
 
 
 /*******************************************
@@ -35,12 +39,15 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
+const tipAmount = billTotal * 0.15
+
+
 
 
 // Print out the tipAmount
 
 
-
+console.log(tipAmount)
 
 /*******************************************
     Iteration 2.2 | Generate Random Number
@@ -48,10 +55,10 @@ const billTotal = 84;
 
 // Generate a random integer between 1 and 10 (inclusive)
 
-
+const randonNumber = Math.floor(Math.random() *10) +1
 // Print the generated random number
 
-
+console.log(randonNumber)
 
 /*******************************************
     Iteration 3.1 | Booleans
@@ -74,3 +81,18 @@ const expression5 = !a || !b;
 const expression6 = !(a || b);
 
 const expression7 = a && a;
+
+
+console.log(expression1)
+
+console.log(expression2)
+
+console.log(expression3) 
+
+console.log(expression4) 
+
+console.log(expression5) 
+
+console.log(expression6) 
+
+console.log(expression7) 
